c18676da8d5e654fe4fbce01b1c496e918dcbf05
Facundo Silvera <facusilvera1410@outlook.com>
2023-07-28 16:09:47 -0300

    Add url backend

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { AuthProvider  } from './context/AuthProvider'
 
 
 function App() {
-
+  
   return (
       <BrowserRouter>
         <AuthProvider>
--- src/config/axiosURL.jsx
+++ src/config/axiosURL.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const clienteURL = axios.create({
-    baseURL: 'http://localhost:4000/inisa'
+    baseURL: import.meta.env.VITE_BACKEND_URL
 })
 
 export default clienteURL
